70a403655b63c65efa4f96461e01b9ae2d7cde47
Christopher Ventura <chrisventura@outlook.com.br>
2022-06-27 18:23:55 -0300

    .

--- app/src/pages/Home/style.ts
+++ app/src/pages/Home/style.ts
@@ -3,9 +3,9 @@ import styled, { css } from "styled-components";
 export const Home = styled.section`
 	${({ theme }) => css`
 		background-color: ${theme.colors.baseBg1};
+		color: ${theme.colors.textColor};
 		min-width: 100vw;
 		min-height: 100vh;
-		color: ${theme.colors.textColor};
 		display: flex;
 		justify-content: space-between;
 	`}
